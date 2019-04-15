be82543f5567f6f3c82b755f8bc698b4bc3d2fdf
Jake Keena <jacobkeena@users.noreply.github.com>
2019-04-15 14:36:12 -0400

    Updated Resource Dictionaries namespace (Resources -> ResourceDictionaries)

--- JobTrackerBeta/Properties/AssemblyInfo.cs
+++ JobTrackerBeta/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JobTrackerBeta")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Resource to track your job search process.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JobTrackerBeta")]
